2a042eaee103580ea504d207c4cdcb31f275ecc0
b44ken <bradboratto@gmail.com>
2023-12-05 23:57:00 -0500

    fix template

--- template/sol.py
+++ template/sol.py
@@ -7,10 +7,8 @@ if len(sys.argv) < 2:
 input = open(sys.argv[1], 'r').read()
 lines = input.split('\n\n')
 
-seeds = [int(i) for i in re.findall(r'\d+', lines[0])]
-
-# for map in lines:
-#     print(map)
+for line in lines:
+    pass
 
 file_name = sys.argv[1].replace(".txt", "")
 file_padding = " " * (10 - len(file_name))
